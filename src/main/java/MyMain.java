49e39bf2e2163e2454d73f1c610b6b62285e1192
catcherindarah <22rray@athenian.org>
2020-10-08 05:01:22 +0000

    kinda completed the task.. bit confused for the second two

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,22 +5,62 @@ public class MyMain {
     // Returns in the answer as a double corresponding to the percentage
     // For example, 75.5% would be 75.5
     public static double probabilityOneSix() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int rounds = 10000;
+        int six = 0;
+        for (int i = 0; i < rounds; i++){
+            boolean success = false;
+            for (int h = 0; h < 6; h++){
+                if ((int)(Math.random()*6) + 1 == 6){
+                    success = true;
+                }
+            }
+            if (success)
+                six++;
+        }
+        double x = (double)six / rounds;
+        double percent = (double) x * 100;
+        return percent;
+
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int rounds = 10000;
+        int six = 0;
+        for (int i = 0; i < rounds; i++){
+            
+            for (int h = 0; h < 6; h++){
+                if ((int)(Math.random()*6) + 1 == 6){
+                    six++
+                }
+            }
+
+        }
+        double x = (double)six / rounds;
+        double percent = (double) x * 100;
+        return percent;
+
     }
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int rounds = 10000;
+        int six = 0;
+        for (int i = 0; i < rounds; i++){
+            
+            for (int h = 0; h < 6; h++){
+                if ((int)(Math.random()*6) + 1 == 6){
+                    six++
+                }
+            }
+
+        }
+        double x = (double)six / rounds;
+        double percent = (double) x * 100;
+        return percent;
+
     }
 
 
